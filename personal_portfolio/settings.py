7af989a2fd1750f2061dd1f9b17c1ce7db707b7c
Paritosh Tripathi <tripathiparitosh935@gmail.com>
2022-06-15 17:36:45 +0530

    Automatic configuration with django-heroku

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -137,3 +137,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'personal_portfolio/assets')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import django_heroku
+django_heroku.settings(locals())
